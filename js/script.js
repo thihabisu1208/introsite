415feac15e3988e9414ee464286ada01c8bdceaf
thihabisu1208 <39027275+thihabisu1208@users.noreply.github.com>
2018-06-16 23:09:55 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,56 @@
+//Return to Top
 $(function($){
     $('.totop').pageTop();
     }
 );
+
+
+//Smooth Scroll Anchor Links
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+// Sticky Navigation with Scroll
+(function($) {          
+  $(document).ready(function(){                    
+      $(window).scroll(function(){                          
+          if ($(this).scrollTop() > 200) {
+              $('#navigation').fadeIn(500);
+          } else {
+              $('#navigation').fadeOut(400);
+          }
+      });
+  });
+})(jQuery);
